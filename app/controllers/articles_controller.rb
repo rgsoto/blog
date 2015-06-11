42532cadab4d4f092cf178ae022ff7f66f8c2f74
Reni <your.rgabrielsoto@gmail.com>
2015-06-11 07:52:26 -0500

    Did 5.3 to 5.6 of the rails getting started doc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,10 @@ class ArticlesController < ApplicationController
  
   def create
     # last two comments are from stack overflow solutions 
-    render plain: params[:article].inspect
-    #@article = Article.new(article_params)
-    #@article.save
+    #render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article.save
+    redirect_to @article 
   end
 
   def index 
