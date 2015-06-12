2b08ca82d7de2de9bd4a8d19367450e1453a082b
Reni <your.rgabrielsoto@gmail.com>
2015-06-12 07:55:39 -0500

    Rails 5.7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,26 @@
 class ArticlesController < ApplicationController
+ def show
+    @article = Article.find(params[:id])
+  end
  def new
   end
  
   def create
     # last two comments are from stack overflow solutions 
     #render plain: params[:article].inspect
-    @article = Article.new(params.require(:article).permit(:title, :text))
-    @article.save
-    redirect_to @article 
+    
+  @article = Article.new(article_params)
+  @article.save
+  redirect_to @article
+end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
   end
 
-  def index 
-  end 
-end
+  #def index 
+  #end 
+#end
 
