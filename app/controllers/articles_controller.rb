25a8ac15c077c40c7ddcf8ab469457806d14fb54
Reni <your.rgabrielsoto@gmail.com>
2015-06-13 13:28:53 -0500

    Completed 5.8 - 5.11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,24 @@
 class ArticlesController < ApplicationController
+  def index 
+    @articles = Article.all
+  end 
+
  def show
     @article = Article.find(params[:id])
   end
+
  def new
+  @article = Article.new
   end
  
   def create
-    # last two comments are from stack overflow solutions 
-    #render plain: params[:article].inspect
-    
-  @article = Article.new(article_params)
-  @article.save
-  redirect_to @article
+   @article = Article.new(article_params)
+   
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
 end
  
 private
