7e5d72447dca78781fcb8927b8f1ae7a544c5c44
Reni <your.rgabrielsoto@gmail.com>
2015-06-23 07:30:52 -0500

    6.2 - 6.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :articles
+  resources: comments 
   root 'welcome#index'
   end
   # The priority is based upon order of creation: first created -> highest priority.
